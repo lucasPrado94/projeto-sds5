0b9948db9492819e03b652698991b6ddbdf008e1
Lucas Prado <lukep.onbass@gmail.com>
2021-11-03 10:43:06 -0300

    Adding layers to project

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -21,7 +21,11 @@ public class Seller {
 	
 	@OneToMany(mappedBy = "seller")
 	private List<Sale> sales = new ArrayList<>();
-
+	
+	public Seller() {
+		
+	}
+	
 	public Seller(Long id, String name) {
 		this.id = id;
 		this.name = name;
